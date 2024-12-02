239a71a2490a549794f33664505f23ace2701adf
Avish Jha <58023328+avishj@users.noreply.github.com>
2024-12-02 16:24:03 +0000

    fix: change name to team name

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -11,7 +11,7 @@ public class QueryProcessor {
         }
 
         if (query.contains("your name")) {
-            return "RobTest";
+            return "Avish, Anila, Ramya, Prahanya";
         }
 
         return "";
