50fefc34d2ccf4d8f1b0335bd09b370f2bc07329
Avish Jha <58023328+avishj@users.noreply.github.com>
2024-12-02 16:42:06 +0000

    updates

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -4,6 +4,13 @@ public class QueryProcessor {
 
     public String process(String query) {
 
+        if (query.toLowerCase().contains("plus")) {
+            String[] numbers = input.split(" plus ");
+            int num1 = Integer.parseInt(numbers[0].trim());
+            int num2 = Integer.parseInt(numbers[1].trim());
+            return num1 + num2 + "";
+        }
+
         if (query.toLowerCase().contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
